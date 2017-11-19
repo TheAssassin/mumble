98d2786b9868245a742f10475ff6f019ced9bab4
TheAssassin <theassassin@users.noreply.github.com>
2017-11-19 14:14:38 +0100

    Fix build script

--- scripts/travis-ci/build_appimage.bash
+++ scripts/travis-ci/build_appimage.bash
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-qmake CONFIG+=release -recursive
+qmake CONFIG+="release no-server" -recursive
 make -j$(nproc)
 mkdir AppDir
 make INSTALL_ROOT=AppDir install
@@ -15,6 +15,10 @@ mkdir -p AppDir/usr/share/{applications,icons/hicolor/256x256}
 cp scripts/mumble.desktop AppDir/usr/share/applications/
 # TODO: add real icon
 touch AppDir/usr/share/icons/hicolor/256x256/mumble.png
+
+# move release data to usr/bin/ for now
+mkdir -p AppDir/usr/bin
+cp -r release/* AppDir/usr/bin
 
 wget -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 
@@ -28,4 +32,4 @@ unset LD_LIBRARY_PATH
 # add Git commit ID to AppImage filename
 export VERSION=$(git rev-parse --short HEAD)
 ./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/mumble.desktop -bundle-non-qt-libs
-./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/mumble.desktop -appimage
