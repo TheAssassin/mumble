269184e709df565585883e61568553751dbf164c
TheAssassin <theassassin@users.noreply.github.com>
2017-11-19 01:13:59 +0100

    Use all available CPU cores

--- scripts/travis-ci/script.bash
+++ scripts/travis-ci/script.bash
@@ -11,13 +11,13 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 		if [ ${MUMBLE_NO_PCH} -eq 1 ]; then
 			EXTRA_CONFIG="no-pch ${EXTRA_CONFIG}"
 		fi
-		qmake-qt4 CONFIG+="release tests g15-emulator qt4-legacy-compat ${EXTRA_CONFIG}" -recursive && make -j2 && make check
+		qmake-qt4 CONFIG+="release tests g15-emulator qt4-legacy-compat ${EXTRA_CONFIG}" -recursive && make -j$(nproc) && make check
 	elif [ "${MUMBLE_QT}" == "qt5" ] && [ "${MUMBLE_HOST}" == "x86_64-linux-gnu" ]; then
 		EXTRA_CONFIG=
 		if [ ${MUMBLE_NO_PCH} -eq 1 ]; then
 			EXTRA_CONFIG="no-pch ${EXTRA_CONFIG}"
 		fi
-		qmake CONFIG+="release tests g15-emulator ${EXTRA_CONFIG}" -recursive && make -j2 && make check
+		qmake CONFIG+="release tests g15-emulator ${EXTRA_CONFIG}" -recursive && make -j$(nproc) && make check
 	elif [ "${MUMBLE_QT}" == "qt5" ] && [ "${MUMBLE_HOST}" == "i686-w64-mingw32" ]; then
 		wget http://www.steinberg.net/sdk_downloads/asiosdk2.3.zip -P ../
 		unzip ../asiosdk2.3.zip -d ../
@@ -42,7 +42,7 @@ if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
 			EXTRA_CONFIG="no-pch ${EXTRA_CONFIG}"
 		fi
 		${MUMBLE_HOST}.static-qmake-qt5 -recursive -Wall CONFIG+="release tests warnings-as-errors g15-emulator no-overlay no-bonjour no-elevation no-ice ${EXTRA_CONFIG}"
-		make -j2
+		make -j$(nproc)
 		make check TESTRUNNER="wine"
 	else
 		exit 1
@@ -53,7 +53,7 @@ elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
 	export PATH=$PATH:/usr/local/opt/qt5/bin:/usr/local/bin
 	export MUMBLE_PREFIX=/usr/local
 	export MUMBLE_ICE_PREFIX=/usr/local/opt/ice
-	qmake CONFIG+="release tests warnings-as-errors" && make -j2 && make check
+	qmake CONFIG+="release tests warnings-as-errors" && make -j$(nproc) && make check
 else
 	exit 1
 fi
